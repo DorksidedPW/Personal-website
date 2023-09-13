b82240ff28a5133a27be9a05e8e0e2aa198e9d40
Paul Wassen <109725777+DorksidedPW@users.noreply.github.com>
2023-09-13 14:40:46 +0000

    Configure JSS theme

--- personal-site/src/Site.js
+++ personal-site/src/Site.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import Main from './components/layout/main';
 
-const Site = () => {
+const Site = (props) => {
   const[globalData, setGlobalData] = useState(null);
-
+  
   useEffect(() => {
     async function getData() {
       const response = await fetch('https://www.paulwassen.nl/wp-json');
--- personal-site/src/components/layout/main.js
+++ personal-site/src/components/layout/main.js
@@ -1,7 +1,5 @@
 const Main = (props) => {
-  console.log(props)
-
   return(<div>MAIN</div>)
 }
 
-export default Main;
+export default Main;  
