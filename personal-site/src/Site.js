2f4cf7ae1655f23723f415156b8f1f0513fd3a0a
Paul Wassen <109725777+DorksidedPW@users.noreply.github.com>
2023-09-13 12:16:15 +0000

    Fetch global wordpress site data

--- personal-site/src/Site.js
+++ personal-site/src/Site.js
@@ -1,4 +1,21 @@
-function Site() {
+import { useEffect, useState } from "react";
+
+const Site = () => {
+  const[globalData, setGlobalData] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch('https://www.paulwassen.nl/wp-json');
+      const data = await response.json();
+
+      setGlobalData(data);
+
+    }
+
+    getData();
+
+  }, [])
+
   return (
     <div>SITE</div>
   );
