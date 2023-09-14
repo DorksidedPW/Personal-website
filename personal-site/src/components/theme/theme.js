2b2425f2bf6b9abed76e62436632d7d6451819c8
Paul Wassen <109725777+DorksidedPW@users.noreply.github.com>
2023-09-14 14:29:38 +0000

    Added comments to theme

--- personal-site/src/components/theme/theme.js
+++ personal-site/src/components/theme/theme.js
@@ -1,8 +1,10 @@
 const theme = {
   palette: {
+    //PRIMARYCOLORS
     primaryColor: '#D91E0D',
     primaryAccentColor: '#F2DFEB',
 
+    //SECONDARY COLORS
     secondaryColor: '#153440',
     secondaryAccentColor: '',
   },
